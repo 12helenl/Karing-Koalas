925ce420e289995fb4fa4dfa4c64562801042d22
Apple <apple@Apples-MacBook-Pro-34.local>
2019-07-17 16:19:23 -0700

    4:19 NK

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var button = $("button");
 
-var supportiveMessages = ["You are the most perfect you there is", "You are enough", "You make a bigger impact than you realize","Nothing can stop you","You are stunning","You deserve as much love and affection as anyone else","You are not alone"]
-
+var supportiveMessages = ["You are the most perfect you there is", "You are enough", "You make a bigger impact than you realize","Nothing can stop you","You are stunning","You deserve as much love and affection as anyone else","You are not alone"];
 // add more later
 
-var stepOne = Math.random()
+var stepOne = Math.random( )
 
 var random = stepOne *7
 var final = Math.floor(random)
@@ -18,8 +17,6 @@ var kindness = supportiveMessages[final]
 console.log(kindness);
 
 var header = $ ('h2');
-
-header.text(kindness);
 
 button.on("click",doSomething);
 
